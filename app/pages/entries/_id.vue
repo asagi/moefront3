499d591dbd1d120cd0b02828a9be10b744e64b70
Keiju Asagi <k.asagi@nfity.com>
2020-02-03 17:52:38 +0900

    feat: Add map component into entry page

--- app/pages/entries/_id.vue
+++ app/pages/entries/_id.vue
@@ -1,19 +1,37 @@
 <template>
-  <section class="container"></section>
+  <section class="container">
+    <div class="top-area">
+      <map-area :powers="powers" :territories="territories" :units="units" />
+    </div>
+  </section>
 </template>
 
 <script>
+import MapArea from '@/components/-map-area'
+
 export default {
+  components: {
+    MapArea
+  },
+  data: () => {
+    return {
+      table: [],
+      powers: [],
+      territories: [],
+      units: []
+    }
+  },
   async asyncData({ $axios, params }) {
-    await $axios
-      .$get(`/api/tables/${params.id}`)
-      .then(res => {
-        console.log(JSON.stringify(res))
-        return { table: res }
-      })
-      .catch(error => {
-        console.log(error.message)
-      })
+    const res = await $axios.$get(`/api/tables/${params.id}`).catch(error => {
+      console.log(error.message)
+    })
+    // console.log(JSON.stringify(res))
+    return {
+      table: res,
+      powers: res.powers,
+      territories: res.territories,
+      units: res.units
+    }
   }
 }
 </script>
