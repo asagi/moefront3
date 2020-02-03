d2ea80792f2a182b80ba5e92a723deb5f19fc720
Keiju Asagi <k.asagi@nfity.com>
2020-02-03 17:53:47 +0900

    fix: Update plugin

--- app/plugins/remove-ads-style.js
+++ app/plugins/remove-ads-style.js
@@ -1,9 +1,12 @@
 export default ({ app }) => {
   app.router.afterEach(() => {
     setTimeout(() => {
-      document.getElementById('__nuxt').removeAttribute('style')
-      document.getElementById('__layout').removeAttribute('style')
-      document.getElementById('__content').removeAttribute('style')
+      document.getElementById('__nuxt') &&
+        document.getElementById('__nuxt').removeAttribute('style')
+      document.getElementById('__layout') &&
+        document.getElementById('__layout').removeAttribute('style')
+      document.getElementById('__content') &&
+        document.getElementById('__content').removeAttribute('style')
     }, 1)
   })
 }
